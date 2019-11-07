0c41daaa5f4d1f35ed03847b6462ab9680c9f6b0
AnonUD4Y <AnonUD4Y@gmail.com>
2019-11-07 15:09:32 +0000

    sudo Requirements skipped for Termux

--- Defs/Checks.py
+++ Defs/Checks.py
@@ -76,7 +76,7 @@ def checkPermissions():
             if os.getuid() == 0:
                 print("{0}Permissions granted!".format(GREEN))
             else:
-                raise PermissionError("{0}Permissions denied! Please run as '{1}sudo{0}'".format(RED, GREEN)) 
+                print("{0}Permissions granted! If possible run as '{1}sudo{0}'".format(RED, GREEN)) 
         elif systemos() == 'Windows':
             if ctypes.windll.shell32.IsUserAnAdmin() != 0:
                 print("{0}Permissions granted!".format(GREEN))
